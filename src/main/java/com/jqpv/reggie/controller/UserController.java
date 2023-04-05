a0449f831faff490884d3d382a0a0f9730fdce43
jqpv <2742909370@qq.com>
2023-04-05 21:19:39 +0800

    第3次提交

--- src/main/java/com/jqpv/reggie/controller/UserController.java
+++ src/main/java/com/jqpv/reggie/controller/UserController.java
@@ -36,55 +36,74 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
-    //获取验证码
-    @PostMapping("/login")
+
+    /**
+     * 获取验证码
+     * @param session
+     * @param user
+     * @return
+     */
+
+    @PostMapping("/sendMsg")
     public R<String> sendMsg(HttpSession session, @RequestBody User user){
         //获取邮箱号
         //相当于发送短信定义的String to
         String email = user.getPhone();
         String subject = "不知道写什么（测试）";
+
         //StringUtils.isNotEmpty字符串非空判断
         if (StringUtils.isNotEmpty(email)) {
+
             //发送一个四位数的验证码,把验证码变成String类型
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
             String text = "【不知道写什么（测试）】您好，您的登录验证码为：" + code + "，请尽快登录";
             log.info("验证码为：" + code);
+
             //发送短信
             userService.sendMsg(email,subject,text);
+
             //将验证码保存到session当中
             session.setAttribute(email,code);
             return R.success("验证码发送成功");
         }
         return R.error("验证码发送异常，请重新发送");
     }
-    //登录
-    @PostMapping("/loginout")
-    //Map存JSON数据
-    public R<User> login(HttpSession session,@RequestBody Map map){
-        //获取邮箱，用户输入的
+
+    /**
+     * 移动端用户登录
+     * @param map
+     * @param session
+     * @return
+     */
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session){
+        log.info(map.toString());
+
+        //获取邮箱号
         String phone = map.get("phone").toString();
-        //获取验证码，用户输入的
+
+        //获取验证码
         String code = map.get("code").toString();
-        //获取session中保存的验证码
-        Object sessionCode = session.getAttribute(phone);
-        //如果session的验证码和用户输入的验证码进行比对,&&同时
-        if (sessionCode != null && sessionCode.equals(code)) {
-            //要是User数据库没有这个邮箱则自动注册,先看看输入的邮箱是否存在数据库
+
+        //从Session中获取保存的验证码
+        Object codeInSession = session.getAttribute(phone);
+
+        //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
+        if(codeInSession != null && codeInSession.equals(code)){
+            //如果能够比对成功，说明登录成功
+
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(User::getPhone,phone);
-            //获得唯一的用户，因为手机号是唯一的
+
             User user = userService.getOne(queryWrapper);
-            //要是User数据库没有这个邮箱则自动注册
-            if (user == null) {
+            if(user == null){
+                //判断当前邮箱号对应的用户是否为新用户，如果是新用户就自动完成注册
                 user = new User();
                 user.setPhone(phone);
                 user.setStatus(1);
-                //取邮箱的前五位为用户名
-                user.setName(phone.substring(0,6));
                 userService.save(user);
             }
-            //不保存这个用户名就登不上去，因为过滤器需要得到这个user才能放行，程序才知道你登录了
-            session.setAttribute("user", user.getId());
+            session.setAttribute("user",user.getId());
             return R.success(user);
         }
         return R.error("登录失败");
--- src/main/resources/front/api/login.js
+++ src/main/resources/front/api/login.js
@@ -1,17 +1,26 @@
-function loginApi(data) {
-    return $axios({
-      'url': '/user/login',
-      'method': 'post',
+    function loginApi(data) {
+        return $axios({
+            'url': '/user/login',
+            'method': 'post',
+            data
+        })
+    }
 
-    })
-  }
+    function sendMsgApi(data) {
+        return $axios({
+            'url': '/user/sendMsg',
+            'method': 'post',
+            data
+        })
+    }
 
-function loginoutApi() {
-  return $axios({
-    'url': '/user/loginout',
-    'method': 'post',
-      data
-  })
-}
+    function loginoutApi() {
+        return $axios({
+            'url': '/user/loginout',
+            'method': 'post',
+        })
+    }
+
+
 
   
